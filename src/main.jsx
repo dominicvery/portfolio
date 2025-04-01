9d7cc7525d0c3fef310d27c657d2f89bd553c3e9
Dominic Veryard <dominicveryard@Dominics-MacBook-Air.local>
2025-04-01 10:22:48 +0100

    removed gtm header

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import TagManager from 'react-gtm-module';
-
-const tagManagerArgs = {
-  gtmId: "G-4XS9THNKWY", 
-};
-
-TagManager.initialize(tagManagerArgs);
 
 TagManager.initialize(tagManagerArgs);
 ReactDOM.createRoot(document.getElementById('root')).render(
